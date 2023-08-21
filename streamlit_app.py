b4c24dcba4730b50c4e9d56fee755e182ceb5e0e
ryoutaku <ryotaku.nakagawa@gmail.com>
2023-08-22 02:19:30 +0900

    tmp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,35 @@
-import openai
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 import streamlit as st
 
+import openai
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+documents = SimpleDirectoryReader("files").load_data()
+index = GPTVectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[株式会社マネーフォワード-決算説明資料](https://corp.moneyforward.com/ir/library/presentation/)"
 
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "マネーフォワードの2023年11月期の決算情報について回答できます"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
+    response = query_engine.query(prompt)
+    msg = response.response
     st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    st.chat_message("assistant").write(msg)
